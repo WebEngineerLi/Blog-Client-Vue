50628d1eab876ce7b2e0dea2fa09e3de10fe8c8c
wb-lmy412939 <wb-lmy412939@alibaba-inc.com>
2019-07-23 19:05:24 +0800

    init: 初始化项目

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="header">
+		{{ title }}
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		title: String
+	}
+}
+</script>
+<style scoped>
+@import './header.less';
+</style>
---
+++ src/views/Blog.vue
@@ -0,0 +1,14 @@
+<template>
+  <headers title="这是Header"></headers>
+</template>
+
+<script>
+import Headers from '@/components/Header';
+export default {
+  name: 'blog',
+
+  components: {
+    Headers
+  }
+}
+</script>
